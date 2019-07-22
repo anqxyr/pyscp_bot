a7a710de9a9880efad4af0b2a7257806be73176c
Ross Williams <rossjrw@gmail.com>
2019-07-23 00:30:03 +0100

    Create ignore.py

---
+++ jarvis/ignore.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+###############################################################################
+# Module Imports
+###############################################################################
+
+from . import core, lex, parser, db
+
+###############################################################################
+
+db.init('jarvis.db')
+
+###############################################################################
+# Commands
+###############################################################################
+
+@core.command
+@parser.ignore
+def ignore(inp, *, user):
+    inst = db.Ignored.find_one(user=user)
+    if inst:
+        db.Ignored.purge(user=user)
+        return lex.ignore.unignored(user=user)
+    else:
+        db.Ignored.create(user=user)
+        return lex.ignore.ignored(user=user)
